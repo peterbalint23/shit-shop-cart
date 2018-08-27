8752dd1db3b8191c982e0f948d86a3d5772e5bb7
Balint Peter <balint.peter23@gmail.com>
2018-08-27 14:48:48 +0200

    project base

--- src/main/java/com/codecool/shitshopcart/ShitShopCartApplication.java
+++ src/main/java/com/codecool/shitshopcart/ShitShopCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShitShopCartApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ShitShopCartApplication.class, args);
     }
 }
