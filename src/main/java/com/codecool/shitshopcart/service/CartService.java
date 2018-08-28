12a658f953acbd304fe27d1d0fba75a232de7ce1
racztamara97 <racztamara97@gmail.com>
2018-08-28 15:18:34 +0200

    Find cart by user_id

--- src/main/java/com/codecool/shitshopcart/service/CartService.java
+++ src/main/java/com/codecool/shitshopcart/service/CartService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class CartService {
 
@@ -17,7 +19,13 @@ public class CartService {
         Cart cart = cartRepository.findByUser_id(user_id);
         cart.getProducts().add(product_id);
         cartRepository.save(cart);
+    }
 
+    @Transactional
+    public List<Long> findCartByUserId(long user_id){
+        Cart cart = cartRepository.findByUser_id(user_id);
+        List<Long> products = cart.getProducts();
+        return products;
     }
 }
 
