3ca54b6b5c37a523d3c49ae15eefee258d263f95
Balint Peter <balint.peter23@gmail.com>
2018-08-28 14:58:02 +0200

    add to cart by user_id

--- src/main/java/com/codecool/shitshopcart/controller/CartController.java
+++ src/main/java/com/codecool/shitshopcart/controller/CartController.java
@@ -1,22 +1,31 @@
 package com.codecool.shitshopcart.controller;
 
+import com.codecool.shitshopcart.repository.CartRepository;
+import com.codecool.shitshopcart.service.CartService;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class CartController {
 
+    @Autowired
+    private CartService cartService;
 
 
-        @GetMapping("/cart")
-        public JSONObject getUser(){
-            JSONObject obj = new JSONObject();
-            obj.put("name", "Szabolcs");
 
-            return obj;
-        }
+    @GetMapping("/cart")
+    public JSONObject getUser() {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "Szabolcs");
+
+        return obj;
+    }
+
+    @PutMapping("/cart/{user_id}/{product_id}")
+    public void addToCart(@PathVariable("user_id") long user_id, @PathVariable("product_id") long product_id) {
+        cartService.addToCart(user_id, product_id);
+    }
 
 }
--- src/main/java/com/codecool/shitshopcart/repository/CartRepository.java
+++ src/main/java/com/codecool/shitshopcart/repository/CartRepository.java
@@ -2,6 +2,13 @@ package com.codecool.shitshopcart.repository;
 
 import com.codecool.shitshopcart.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository <Cart, Long> {
+
+    Cart findByUser_id(long user_id);
 }
